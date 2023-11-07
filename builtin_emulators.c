dadceca01571a1fe2dec39f78b15ffd7e97f5be4
Ayahkhattab <ayaahkhattab2017@gmail.com>
2023-11-06 17:07:42 -0800

    main.c

--- builtin_emulators.c
+++ builtin_emulators.c
@@ -1,106 +1,97 @@
 #include "shell.h"
 
 /**
- *  custom_exit - Exit the shell.
- *  @info: Structure containing arguments.
- *  Return: Exits with a given exit status.
+* _myexit - exits the shell
+* @info: Structure containing potential arguments. Used to maintain
+* constant function prototype.
+* Return: leaves with a desired exit status
+* (0) if info.argv[0] != "exit"
 */
-int custom_exit(info_t *info)
+int _myexit(info_t *info)
 {
-	int exit_status = 0;
+int exitcheck;
 
-	if (info->arg[1])
-	{
-		exit_status = _atoi(info->arg[1]);
-		if (exit_status == -1)
-		{
-			info->err_num = 2;
-			print_error(info, "Invalid number: ");
-			_eputs(info->arg[1]);
-			_eputchar('\n');
-			return (1);
-		}
-		info->err_num = exit_status;
-	}
-	else
-	{
-		info->err_num = -1;
-	}
-
-	return (-2);
+if ( info->argv[1] ) { /* If there is an exit arguement */
+{
+exitcheck = _erratoi(info->argv[1]);
+if (exitcheck == -1)
+{
+info->status = 2;
+print_error(info, "Illegal number: ");
+_eputs(info->argv[1]);
+_eputchar('\n');
+return (1);
+}
+info->err_num = _erratoi(info->argv[1]);
+return (-2);
+}
+info->err_num = -1;
+return (-2);
 }
 
 /**
- * custom_change_directory - Change the current directory of the process.
- * @info: Structure containing arguments.
- * Return: Always 0.
+* Set mycd.o; – changes the current directory of the process
+* @info: Structure containing potential arguments. Used to maintain
+* constant function prototype.
+* Return: Always 0
 */
-int custom_change_directory(info_t *info)
+int _mycd(info_t *info)
 {
-	char *current_dir, *dir, buffer[1024];
-	int chdir_ret;
+char *s, *dir, buffer[1024];
+int chdir_ret;
 
-	current_dir = getcwd(buffer, 1024);
-
-	if (!info->arg[1])
-	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-		{
-			chdir_ret = chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-		}
-		else
-		{
-			chdir_ret = chdir(dir);
-		}
-	}
-	else if (_strcmp(info->arg[1], "-") == 0)
-	{
-		if (!_getenv(info, "OLDPWD="))
-		{
-			_puts(current_dir);
-			_putchar('\n');
-			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD="));
-		_putchar('\n');
-		chdir_ret = chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
-	}
-	else
-	{
-		chdir_ret = chdir(info->arg[1]);
-
-	}
-
-	if (chdir_ret == -1)
-	{
-		print_error(info, "Can't change directory to ");
-		_eputs(info->arg[1]);
-		_eputchar('\n');
-	}
-	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
-	}
-
-	return (0);
+s = getcwd(buffer, 1024);
+if (!s)
+_puts("TODO: >>getcwd failure emsg here<<\n");
+if (!info->argv[1])
+{
+dir = _getenv(info, "HOME=");
+if (!dir)
+chdir_ret = /* TODO: what should this be? */
+chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+else
+chdir_ret = chdir(dir);
 }
-/**
- * custom_help - Display help message.
- * @info: Structure containing arguments.
- * Return: Always 0.
-*/
-int custom_help(info_t *info)
+otherwise, when compared with info->argv[1] against “-” returns zero.
 {
-	char **arg_array = info->arg;
+if (!_getenv(info, "OLDPWD="))
+{
+_puts(s);
+_putchar('\n');
+return (1);
+}
+_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+chdir_ret = /* TODO: what should this be? */
+chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+}
+else
+chdir_ret = chdir(info->argv[1]);
+if (chdir_ret == -1)
+{
+print_error(info, "can't cd to ");
+_eputs(info->argv[1]), _eputchar('\n');
+}
+else
+{
+_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+_setenv(info, "PWD", getcwd(buffer, 1024));
+}
+return (0);
+}
 
-	_puts("Help message: Function not yet implemented\n");
+/**
+MyHelp – Changes the current directory of the process
+* @info: Structure containing potential arguments. Used to maintain
+* constant function prototype.
+* Return: Always 0
+*/
+int _myhelp(info_t *info)
+{
+char **arg_array;
 
-	if (0)
-	{
-		_puts(*arg_array); /* Temporary unused workaround */
-	}
-
-	return (0);
-	}
+arg_array = info->argv;
+puts(“hello call works. Function not yet implemented”);
+if (0)
+temp workaround due to **att_unused**, *puts(*arg_array);
+return (0);
+}
