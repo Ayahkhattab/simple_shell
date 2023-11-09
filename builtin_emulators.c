b9fcb52e75d63011b3275b723ee11dbf64d683e7
Ayahkhattab <ayaahkhattab2017@gmail.com>
2023-11-08 16:44:36 -0800

    builtin_emulators.c

--- builtin_emulators.c
+++ builtin_emulators.c
@@ -1,97 +1,139 @@
 #include "shell.h"
 
 /**
-* _myexit - exits the shell
-* @info: Structure containing potential arguments. Used to maintain
-* constant function prototype.
-* Return: leaves with a desired exit status
-* (0) if info.argv[0] != "exit"
+ * custom_exit - Exit the shell.
+ * @info: Structure containing arguments.
+ *
+ * Description: This function handles the "exit" built-in command.
+ * It exits the shell with a given exit status.
+ *
+ * Return: Exits the shell with a given exit status.
+ * -2 if successful, 1 if there's an error.
 */
-int _myexit(info_t *info)
+int custom_exit(info_t *info)
 {
-int exitcheck;
+	if (info->args[1])
+	{
+	return handle_exit_with_status(info, info->args[1]);
+	}
+	else
+	{
+		info->err_num = -1;
+	}
+	return (-2);
+}
 
-if ( info->argv[1] ) { /* If there is an exit arguement */
+int handle_exit_with_status(info_t *info, char *status_str)
 {
-exitcheck = _erratoi(info->argv[1]);
-if (exitcheck == -1)
-{
-info->status = 2;
-print_error(info, "Illegal number: ");
-_eputs(info->argv[1]);
-_eputchar('\n');
-return (1);
-}
-info->err_num = _erratoi(info->argv[1]);
-return (-2);
-}
-info->err_num = -1;
-return (-2);
+	int exit_status = _atoi(status_str);
+	if (exit_status == -1)
+
+	{
+		info->err_num = -2;
+		print_error(info, "Invalid number: ");
+		_eputs(status_str);
+		_eputchar('\n');
+		return (1);
+	}
+	info->err_num = exit_status;
+	return (-2);
 }
 
 /**
-* Set mycd.o; – changes the current directory of the process
-* @info: Structure containing potential arguments. Used to maintain
-* constant function prototype.
-* Return: Always 0
+ * custom_change_directory - Change the current directory of the process.
+ * @info: Structure containing arguments.
+ *
+ * Return: Always 0.
 */
-int _mycd(info_t *info)
+int custom_change_directory(info_t *info)
 {
-char *s, *dir, buffer[1024];
-int chdir_ret;
+	char *current_dir, buffer[1024];
+	int chdir_ret;
 
-s = getcwd(buffer, 1024);
-if (!s)
-_puts("TODO: >>getcwd failure emsg here<<\n");
-if (!info->argv[1])
-{
-dir = _getenv(info, "HOME=");
-if (!dir)
-chdir_ret = /* TODO: what should this be? */
-chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-else
-chdir_ret = chdir(dir);
+	current_dir = getcwd(buffer, 1024);
+
+	if (!info->args[1])
+	{
+	return handle_cd_with_no_arguments(info);
+	}
+	else if (_strcmp(info->args[1], "-") == 0)
+	{
+	return handle_cd_with_dash_argument(info, current_dir);
+	}
+	else
+	{
+		chdir_ret = chdir(info->args[1]);
+	}
+
+	if (chdir_ret == -1)
+	{
+		print_error(info, "Can't change directory to ");
+		_eputs(info->args[1]);
+		_eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
 }
-otherwise, when compared with info->argv[1] against “-” returns zero.
+
+
+int handle_cd_with_no_arguments(info_t *info)
 {
-if (!_getenv(info, "OLDPWD="))
+	char *dir;
+	int chdir_ret;
+
+	dir = _getenv(info, "HOME=");
+
+
+	if (!dir)
+	{
+		chdir_ret = chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+	}
+	else
+	{
+		chdir_ret = chdir(dir);
+	}
+
+	return (0);
+}
+
+int handle_cd_with_dash_argument(info_t *info, char *current_dir)
 {
-_puts(s);
-_putchar('\n');
-return (1);
-}
-_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-chdir_ret = /* TODO: what should this be? */
-chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
-}
-else
-chdir_ret = chdir(info->argv[1]);
-if (chdir_ret == -1)
-{
-print_error(info, "can't cd to ");
-_eputs(info->argv[1]), _eputchar('\n');
-}
-else
-{
-_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-_setenv(info, "PWD", getcwd(buffer, 1024));
-}
-return (0);
+	if (!_getenv(info, "OLDPWD="))
+	{
+		_puts(current_dir);
+		_putchar('\n');
+		return (1);
+	}
+	_puts(_getenv(info, "OLDPWD="));
+	_putchar('\n');
+	chdir_ret = chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	return (0);
 }
 
 /**
-MyHelp – Changes the current directory of the process
-* @info: Structure containing potential arguments. Used to maintain
-* constant function prototype.
-* Return: Always 0
+ * custom_help - Display help message.
+ * @info: Structure containing arguments.
+ *
+ * Description: This function handles the "help" built-in command.
+ * It displays a help message.
+ *
+ * Return: Always 0.
 */
-int _myhelp(info_t *info)
+int custom_help(info_t *info)
 {
-char **arg_array;
+	char **arg_array;
 
-arg_array = info->argv;
-puts(“hello call works. Function not yet implemented”);
-if (0)
-temp workaround due to **att_unused**, *puts(*arg_array);
-return (0);
+	arg_array = info->args;
+	_puts("Help message: Function not yet implemented\n");
+	if (0)
+	{
+		_puts(*arg_array); /* Temporary unused workaround */
+	}
+
+	return (0);
 }
+
